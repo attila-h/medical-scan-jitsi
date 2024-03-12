fbe03d5a2f32605429bdaf508b1f228770988237
Hauth Attila <hauth.attila@gmail.com>
2024-03-13 00:06:56 +0100

    Prepare containers for actions and events

--- jitsi.js
+++ jitsi.js
@@ -1,9 +1,18 @@
-const domain = 'meet.jit.si';
-const options = {
-    roomName: 'MedicalScanJitsyRoom',
-    width: 700,
-    height: 700,
-    parentNode: document.querySelector('#jitsi-container'),
-    lang: 'hu'
-};
-const api = new JitsiMeetExternalAPI(domain, options);
+let api;
+const actionContainer = document.querySelector('#jitsi-action-container')
+const eventContainer = document.querySelector('#jitsi-event-container')
+
+const initJitsyIframe = () => {
+  const domain = 'meet.jit.si';
+  const options = {
+      roomName: 'MedicalScanJitsyRoom',
+      width: '100%',
+      height: '100%',
+      parentNode: document.querySelector('#jitsi-container'),
+      lang: 'hu',
+  };
+  
+  api = new JitsiMeetExternalAPI(domain, options);
+}
+
+initJitsyIframe();
