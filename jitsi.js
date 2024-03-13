d753bc69d02b12cfa9c33db57260e161eea83981
Hauth Attila <hauth.attila@gmail.com>
2024-03-13 10:18:36 +0100

    Implement some API interaction

--- jitsi.js
+++ jitsi.js
@@ -2,6 +2,33 @@ let api;
 const actionContainer = document.querySelector('#jitsi-action-container')
 const eventContainer = document.querySelector('#jitsi-event-container')
 
+const initGenerateNameButton = () => {
+  const button = document.createElement('button');
+  button.innerHTML = 'Random név generátor';
+  console.log(api)
+  button.onclick = () => api.executeCommand('displayName', Math.random().toString(36).slice(2));
+  actionContainer.appendChild(button);
+}
+
+const showEventInfo = (info) => {
+  let div = document.createElement('div');
+  div.innerHTML = info
+  eventContainer.appendChild(div)
+} 
+
+const initEvents = () => {
+  api.addListener('videoConferenceJoined', (event) => {
+    console.log(event);
+    showEventInfo(`${event.formattedDisplayName} joined to ${event.roomName}`)
+  });
+  
+  api.addListener('audioMuteStatusChanged', (event) => {
+    const isMuted = event.muted;
+    showEventInfo(`Audio mute status changed to ${isMuted}`)
+    console.log(`Audio mute status changed to ${isMuted}`);
+  });
+}
+
 const initJitsyIframe = () => {
   const domain = 'meet.jit.si';
   const options = {
@@ -15,12 +42,8 @@ const initJitsyIframe = () => {
   api = new JitsiMeetExternalAPI(domain, options);
 }
 
-const initGenerateNameButton = () => {
-  let button = document.createElement('button');
-  button.innerHTML = 'Random név generátor';
-  button.onclick = () => api.executeCommand('displayName', Math.random().toString(36).slice(2));
-  actionContainer.appendChild(button);
-}
-
+console.info("Init custom jitsi script")
 initJitsyIframe();
 initGenerateNameButton();
+initEvents()
+
